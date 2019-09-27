571b74a17086ffc34a2ced82e7de49dbc73e3d59
visual_community@outlook.com <visual_community@outlook.com>
2019-09-27 18:13:21 -0400

    commit2

---
+++ WebEval/WebEval/Models/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebEval.Models
+{
+    public class DataContext: DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<WebEval.Models.Estudiante> Estudiantes { get; set; }
+
+        public System.Data.Entity.DbSet<WebEval.Models.Nota> Notas { get; set; }
+    }
+}
