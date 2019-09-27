a0cacbccd30ac32137ddbd12d2a448eb780d2e0f
visual_community@outlook.com <visual_community@outlook.com>
2019-09-27 18:26:56 -0400

    api

---
+++ WebEval/apiEval/Models/Estudiante.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace apiEval.Models
+{
+    public enum Gender
+    {
+        Femenino,
+        Masculino,
+
+    }
+    public class Estudiante
+    {
+        [Key]
+        [Required]
+        public int StudentID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public Gender Gender { get; set; }
+        [Required]
+        public int Birthday { get; set; }
+
+        public virtual ICollection<Nota> Notas { get; set; }
+    }
+}
---
+++ WebEval/apiEval/Models/Nota.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apiEval.Models
+{
+    public enum Estado
+    {
+        Aprobado,
+        Reprobado,
+    }
+    public class Nota
+    {
+        [Key]
+        public int StudentID { get; set; }
+        [Required]
+        public int Note { get; set; }
+        [Required]
+        public int Materia { get; set; }
+        [Required]
+        public Estado Estado { get; set; }
+
+        public Estudiante Estudiante { get; set; }
+
+    }
+}
