f03c2db8c88108063ae7cac1388714a48b95e9e9
visual_community@outlook.com <visual_community@outlook.com>
2019-09-27 18:02:06 -0400

    commit1

---
+++ WebEval/WebEval/Models/Nota.cs
@@ -0,0 +1,26 @@
+﻿
+namespace WebEval.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    public enum Estado
+    {
+        Aprobado,
+        Reprobado,
+    }
+    public class Nota
+    {
+        [Key]
+      
+        public int StudentID { get; set; }
+        [Required]
+        public int Note { get; set; }
+        [Required]
+        public int Materia { get; set; }
+        [Required]
+        public Estado Estado { get; set; }
+
+        public Estudiante Estudiante { get; set; }
+        
+    }
+}
